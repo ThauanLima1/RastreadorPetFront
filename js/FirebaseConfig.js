701bce2d383641d4a49c72736191911444bdcbf9
ThauanLima1 <mitoduxmito@gmail.com>
2025-11-01 16:01:52 -0300

    Adiciona rastreador pet

--- js/FirebaseConfig.js
+++ js/FirebaseConfig.js
@@ -2,14 +2,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCBHpzRuJlMyQ-U4zHJxg5XU4Jb8sn3ScY",
-  authDomain: "rastreadorpet-1f595.firebaseapp.com",
-  projectId: "rastreadorpet-1f595",
-  storageBucket: "rastreadorpet-1f595.appspot.com",
-  messagingSenderId: "801484788651",
-  appId: "1:801484788651:web:ea7a23b01f33bdf181b8ee",
-  measurementId: "G-B6MHYV7TX4"
+  const firebaseConfig = {
+    apiKey: "AIzaSyAk7QRi6OFbIhxLtNrmIDuWJT2PL_wlVxE",
+    authDomain: "petshop-b68eb.firebaseapp.com",
+    databaseURL: "https://petshop-b68eb-default-rtdb.firebaseio.com",
+    projectId: "petshop-b68eb",
+    storageBucket: "petshop-b68eb.firebasestorage.app",
+    messagingSenderId: "727227668021",
+    appId: "1:727227668021:web:887fd37d50a945f4a2c37c"
 };
 
 const app = initializeApp(firebaseConfig);
